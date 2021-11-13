87adb8ccda92576de3aa141b34054ee334bc8b02
Nicholas Tietz-Sokolsky <me@ntietz.com>
2021-11-13 16:38:50 -0500

    Solve problem 3 and add a general method for finding prime factors

---
+++ src/math/factors.rs
@@ -0,0 +1,42 @@
+use crate::math::primes::Primes;
+
+/// Determines the prime factors of a given number. If a factor occurs
+/// multiple times, it will be returned multiple times.
+///
+/// Factors are returned in sorted order.
+pub fn prime_factors(x: u64) -> Vec<u64> {
+    let mut primes = Primes::new();
+    let mut factors = vec![];
+
+    let mut x = x;
+
+    while x > 1 {
+        let factor = primes.next().unwrap();
+
+        while x % factor == 0 {
+            factors.push(factor);
+            x = x / factor;
+        }
+    }
+
+    factors
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factors_13195() {
+        let expected = vec![5, 7, 13, 29];
+        let actual = prime_factors(13195);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn factors_300() {
+        let expected = vec![2, 2, 3, 5, 5];
+        let actual = prime_factors(300);
+        assert_eq!(expected, actual);
+    }
+}
---
+++ src/math/primes.rs
@@ -0,0 +1,56 @@
+pub struct Primes {
+    known: Vec<u64>,
+    curr: usize,
+}
+
+impl Primes {
+    pub fn new() -> Primes {
+        Primes {
+            known: vec![2, 3],
+            curr: 0,
+        }
+    }
+
+    pub fn refill(&mut self) {
+        let max_known = self.known[self.known.len()-1];
+        let chunk_size = 1000;
+
+        let lower = max_known + 2;
+        let upper = lower + chunk_size;
+
+        for n in (lower..upper).step_by(2) {
+            if !self.known.iter().any(|p| n % p == 0) {
+                self.known.push(n)
+            }
+        }
+    }
+}
+
+impl Iterator for Primes {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.curr >= self.known.len() {
+            self.refill();
+        }
+        let result = self.known[self.curr];
+        self.curr += 1;
+
+        Some(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gets_primes() {
+        let expected = vec![2, 3, 5, 7, 11, 13, 17, 19];
+
+        let primes = Primes::new();
+        let actual: Vec<_> = primes.take(8).collect();
+
+        assert_eq!(expected, actual);
+    }
+}
