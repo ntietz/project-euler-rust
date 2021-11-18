f62f26bca6cc65883ddc1f266946d202e1b52a9f
Nicholas Tietz-Sokolsky <me@ntietz.com>
2021-11-17 23:09:45 -0500

    Add solution to problem 5

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -2,3 +2,4 @@ pub mod prob1;
 pub mod prob2;
 pub mod prob3;
 pub mod prob4;
+pub mod prob5;
---
+++ src/problems/prob5.rs
@@ -0,0 +1,31 @@
+pub fn solution() -> u64 {
+    solve(20)
+}
+
+fn solve(limit: u64) -> u64 {
+    let mut nums: Vec<u64> = (1..limit).collect();
+
+    for i in 0..nums.len() {
+        if nums[i] == 1 {
+            continue;
+        }
+
+        for j in (i+1)..nums.len() {
+            if nums[j] % nums[i] == 0 {
+                nums[j] = nums[j] / nums[i];
+            }
+        }
+    }
+
+    nums.iter().fold(1, |a,b| a*b)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verifies_example() {
+        assert_eq!(solve(10), 2520);
+    }
+}
