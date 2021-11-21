06fa42e021592026bc2820754aecd476872a2b53
Nicole Tietz-Sokolskaya <me@ntietz.com>
2021-11-21 10:44:42 -0500

    Add solution to problem 12, speed up factorization through reuse of generated primes

--- src/math/factors.rs
+++ src/math/factors.rs
@@ -1,4 +1,5 @@
 use crate::math::primes::Primes;
+use std::collections::HashMap;
 
 /// Determines the prime factors of a given number. If a factor occurs
 /// multiple times, it will be returned multiple times.
@@ -6,12 +7,18 @@ use crate::math::primes::Primes;
 /// Factors are returned in sorted order.
 pub fn prime_factors(x: u64) -> Vec<u64> {
     let mut primes = Primes::new();
+    prime_factors_memoize(x, &mut primes)
+}
+
+pub fn prime_factors_memoize(x: u64, primes: &mut Primes) -> Vec<u64> {
     let mut factors = vec![];
 
     let mut x = x;
+    let mut idx = 0;
 
     while x > 1 {
-        let factor = primes.next().unwrap();
+        let factor = primes.get(idx);
+        idx += 1;
 
         while x % factor == 0 {
             factors.push(factor);
@@ -20,6 +27,24 @@ pub fn prime_factors(x: u64) -> Vec<u64> {
     }
 
     factors
+}
+
+pub fn num_factors(x: u64) -> u64 {
+    let mut primes = Primes::new();
+    num_factors_memoize(x, &mut primes)
+}
+
+pub fn num_factors_memoize(x: u64, primes: &mut Primes) -> u64 {
+    let factors = prime_factors_memoize(x, primes);
+
+    let mut factor_counts: HashMap<u64, u64> = HashMap::new();
+
+    for factor in factors {
+        let count = factor_counts.entry(factor).or_insert(0);
+        *count += 1;
+    }
+
+    factor_counts.values().map(|c| c + 1).product()
 }
 
 #[cfg(test)]
@@ -38,5 +63,20 @@ mod tests {
         let expected = vec![2, 2, 3, 5, 5];
         let actual = prime_factors(300);
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn checks_num_factors_of_28() {
+        let expected = 6;
+        let actual = num_factors(28);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn checks_num_factors_of_large_num() {
+        let num = 2 * 2 * 3 * 3 * 3 * 5 * 7 * 11 * 11;
+        let expected = (1..=num).filter(|&n| num % n == 0).count();
+
+        assert_eq!(expected as u64, num_factors(num));
     }
 }
--- src/problems/prob12.rs
+++ src/problems/prob12.rs
@@ -1,3 +1,26 @@
+use crate::math::factors::num_factors_memoize;
+use crate::math::primes::Primes;
+use crate::math::triangle_numbers::TriangleNumbers;
+
 pub fn solution() -> u64 {
-    0
+    solve(500)
+}
+
+fn solve(limit: u64) -> u64 {
+    let mut primes = Primes::new();
+    let mut triangle_numbers = TriangleNumbers::new();
+    triangle_numbers
+        .find(|&n| num_factors_memoize(n, &mut primes) > limit)
+        .unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn verify_example() {
+        let expected = 28;
+        assert_eq!(expected, solve(5));
+    }
 }
